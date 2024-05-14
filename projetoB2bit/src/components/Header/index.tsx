a94490ea4f3ed6b16369997a512bed62de9c3cb7
Waldsson Sacramento <waldsson.dev@gmail.com>
2024-05-14 10:24:06 -0300

    feat: create Header component

---
+++ projetoB2bit/src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header className="w-screen h-16 bg-white flex items-center flex-row-reverse">
+      <button className="w-3/12 md:w-64 h-2/3 bg-blueB2 text-white text-lg font-bold py-2 mr-8 rounded-lg focus:outline-none focus:shadow-outline">
+        Logout
+      </button>
+    </header>
+  )
+}
