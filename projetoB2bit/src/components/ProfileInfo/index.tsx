73256b50e6d0ee4d945aa5d2fd293df4c4ece20a
Waldsson Sacramento <waldsson.dev@gmail.com>
2024-05-14 17:09:36 -0300

    feat: add integration

--- projetoB2bit/src/components/ProfileInfo/index.tsx
+++ projetoB2bit/src/components/ProfileInfo/index.tsx
@@ -1,32 +1,53 @@
+import { useEffect } from 'react'
+import { useUserStore } from '../../store/user/User.store'
+import { useAuthStore } from '../../store/auth/Auth.store'
+
 export default function ProfileInfo() {
+  const { getUser, user, isLoading } = useUserStore()
+  const { tokens } = useAuthStore()
+
+  useEffect(() => {
+    if (tokens) {
+      getUser(tokens.access)
+    }
+  }, [getUser, tokens])
+
+  useEffect(() => {
+    console.log(user, 'user profile')
+  }, [user])
+
   return (
     <div className="w-full md:w-96 bg-white rounded-xl shadow-2xl">
-      <div className="w-full flex flex-col items-center">
-        <div className="w-full flex flex-col items-center my-8">
-          <p className="mb-2">Profile picture</p>
-          <img
-            src="https://avatars.githubusercontent.com/u/66279500?v=4"
-            alt="Profile"
-            className="w-14 h-14 rounded-lg"
-          />
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <div className="w-full flex flex-col items-center">
+          <div className="w-full flex flex-col items-center my-8">
+            <p className="mb-2">Profile picture</p>
+            <img
+              src={user?.avatar ? user.avatar : '/profile.png'}
+              alt="Profile avatar"
+              className="w-14 h-14 rounded-lg"
+            />
+          </div>
+          <div className="w-10/12 text-sm mb-5">
+            <p>
+              Your <span className="font-bold">Name</span>
+            </p>
+            <p className="bg-boxGray appearance-none border rounded-lg w-full py-4 px-3 text-textBlack leading-tight focus:outline-none focus:shadow-outline">
+              {user?.name}
+            </p>
+          </div>
+          <div className="w-10/12 text-sm">
+            <p>
+              Your <span className="font-bold">E-mail</span>
+            </p>
+            <p className="bg-boxGray appearance-none border rounded-lg w-full mb-8 py-4 px-3 text-textBlack leading-tight focus:outline-none focus:shadow-outline">
+              {user?.email}
+            </p>
+          </div>
         </div>
-        <div className="w-10/12 text-sm mb-5">
-          <p>
-            Your <span className="font-bold">Name</span>
-          </p>
-          <p className="bg-boxGray appearance-none border rounded-lg w-full py-4 px-3 text-textBlack leading-tight focus:outline-none focus:shadow-outline">
-            Lucas
-          </p>
-        </div>
-        <div className="w-10/12 text-sm">
-          <p>
-            Your <span className="font-bold">E-mail</span>
-          </p>
-          <p className="bg-boxGray appearance-none border rounded-lg w-full mb-8 py-4 px-3 text-textBlack leading-tight focus:outline-none focus:shadow-outline">
-            Lucas
-          </p>
-        </div>
-      </div>
+      )}
     </div>
   )
 }
