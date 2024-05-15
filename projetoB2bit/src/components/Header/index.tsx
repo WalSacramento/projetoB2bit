299a8016c1e5c299249d8277f746e1a8ccb1dca8
Waldsson Sacramento <waldsson.dev@gmail.com>
2024-05-15 09:50:43 -0300

    feat: create handleLogout

--- projetoB2bit/src/components/Header/index.tsx
+++ projetoB2bit/src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import { useAuthStore } from '../../store/auth/Auth.store'
+import useAuthStore from '../../store/auth/Auth.store'
 
 export default function Header() {
   const { logout } = useAuthStore()
@@ -9,7 +9,10 @@ export default function Header() {
 
   return (
     <header className="w-screen h-16 bg-white flex items-center flex-row-reverse">
-      <button className="w-3/12 md:w-64 h-2/3 bg-blueB2 text-white text-lg font-bold py-2 mr-8 rounded-lg focus:outline-none focus:shadow-outline" onClick={handleLogout}>
+      <button
+        className="w-3/12 md:w-64 h-2/3 bg-blueB2 text-white text-lg font-bold py-2 mr-8 rounded-lg focus:outline-none focus:shadow-outline"
+        onClick={handleLogout}
+      >
         Logout
       </button>
     </header>
