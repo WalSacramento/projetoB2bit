6894780b42ffe53b7bbb0c419c28936d83d9cd35
Waldsson Sacramento <waldsson.dev@gmail.com>
2024-05-15 09:48:38 -0300

    feat: add token vericaftion in login page

--- projetoB2bit/src/pages/Home/index.tsx
+++ projetoB2bit/src/pages/Home/index.tsx
@@ -1,12 +1,24 @@
+import { useEffect } from 'react'
 import Header from '../../components/Header'
 import ProfileInfo from '../../components/ProfileInfo'
+import  useAuthStore  from '../../store/auth/Auth.store'
+import { useUserStore } from '../../store/user/User.store'
 
 export default function Home() {
+  const { getUser, user, isLoading } = useUserStore()
+  const { accessToken } = useAuthStore()
+
+  useEffect(() => {
+    if (accessToken) {
+      getUser(accessToken)
+    }
+  }, [getUser, accessToken])
+
   return (
     <div className="h-screen">
       <Header />
       <div className="bg-backgroundHome flex flex-col items-center h-full pt-24 bg-bgGray">
-        <ProfileInfo />
+        <ProfileInfo isLoading={isLoading} user={user} />
       </div>
     </div>
   )
