608c3eea5d37426896670a61a77ae2f57823ab4b
Waldsson Sacramento <waldsson.dev@gmail.com>
2024-05-15 11:13:10 -0300

    feat: add skeleton loading

--- projetoB2bit/src/components/ProfileInfo/index.tsx
+++ projetoB2bit/src/components/ProfileInfo/index.tsx
@@ -1,11 +1,32 @@
-import { Avatar } from 'antd'
+import { Avatar, Skeleton } from 'antd'
 import { ProfileInfoProps } from './ProfileInfo.props'
 
 export default function ProfileInfo({ isLoading, user }: ProfileInfoProps) {
   return (
     <div className="w-10/12 sm:w-2/3 md:w-3/5 lg:w-3/12 xl:w-3/12 bg-white rounded-xl shadow-2xl">
       {isLoading ? (
-        <p>Loading...</p>
+        <div className="w-full flex flex-col items-center">
+          <div className="w-full flex flex-col items-center my-8">
+            <p className="mb-2">Profile picture</p>
+            <Skeleton.Avatar active />
+          </div>
+          <div className="w-10/12 text-sm mb-5">
+            <p>
+              Your <span className="font-bold">Name</span>
+            </p>
+            <div className="bg-boxGray appearance-none border rounded-lg w-full py-2 px-3 text-textBlack leading-tight focus:outline-none focus:shadow-outline">
+              <Skeleton.Input active style={{ width: '100%' }} size="large" />
+            </div>
+          </div>
+          <div className="w-10/12 text-sm mb-8">
+            <p>
+              Your <span className="font-bold">Name</span>
+            </p>
+            <div className="bg-boxGray appearance-none border rounded-lg w-full py-2 px-3 text-textBlack leading-tight focus:outline-none focus:shadow-outline">
+              <Skeleton.Input active style={{ width: '100%' }} size="large" />
+            </div>
+          </div>
+        </div>
       ) : (
         <div className="w-full flex flex-col items-center">
           <div className="w-full flex flex-col items-center my-8">
