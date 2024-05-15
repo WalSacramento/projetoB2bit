8e17a33788830ac614e265422221208ce5852d60
Waldsson Sacramento <waldsson.dev@gmail.com>
2024-05-15 09:44:37 -0300

    feat: add injectZustandInstance to store

--- projetoB2bit/src/store/auth/Auth.store.ts
+++ projetoB2bit/src/store/auth/Auth.store.ts
@@ -1,69 +1,83 @@
-import { create } from "zustand";
-import { LoginStates, LoginStoreProps } from "./Auth.props";
-import { useNavigate } from 'react-router-dom';
-import { loginRequest } from "../../services/requests/auth/auth";
-import { createJSONStorage, persist } from "zustand/middleware";
-
+import { create } from 'zustand'
+import { LoginStates, LoginStoreProps } from './Auth.props'
+import { useNavigate } from 'react-router-dom'
+import { loginRequest } from '../../services/requests/auth/auth'
+import { createJSONStorage, persist } from 'zustand/middleware'
+import { message } from 'antd'
+import { injectZustandInstance } from '../../utils/injectZustandInstance'
 
 const initialState: LoginStates = {
   isLoading: false,
-  tokens: null
+  accessToken: null,
+  refreshToken: null,
+  errorMessage: ''
 }
 
-export const useAuthStore = create(persist<LoginStoreProps> ((set, get) => ({
-  ...initialState,
+const useAuthStore = create(
+  persist<LoginStoreProps>(
+    (set, get) => ({
+      ...initialState,
 
-  login: async (
-    email: string,
-    password: string,
-    navigate: ReturnType<typeof useNavigate>
-  ) => {
-    const { makeAsync } = get();
+      login: async (
+        email: string,
+        password: string,
+        navigate: ReturnType<typeof useNavigate>
+      ) => {
+        const { makeAsync } = get()
 
-    const handle = async (): Promise<void> => {
-      set({ isLoading: true });
-      const response = await loginRequest({ email, password });
-      // Atualize o estado com os novos campos
-      set({tokens: response.tokens, isLoading: false });
-      navigate('/');
-      console.log(response.tokens, 'tokens')
+        const handle = async (): Promise<void> => {
+          set({ isLoading: true })
+
+          const response = await loginRequest({ email, password })
+          set({
+            accessToken: response.tokens.access,
+            refreshToken: response.tokens.refresh,
+            isLoading: false,
+            errorMessage: ''
+          })
+          navigate('/home')
+        }
+
+        const onError = async (): Promise<void> => {
+          message.error({
+            content: 'Usuário ou senha inválidos',
+            duration: 3, 
+            style: {
+              marginTop: '10vh',
+              color: '#ff4d4f',
+              fontWeight: 'bold'
+            }
+          })
+          set({ isLoading: false })
+        }
+
+        void makeAsync({ handle, onError })
+      },
+
+      logout: () => {
+        set({ accessToken: null, refreshToken: null })
+      },
+
+      makeAsync: async ({ handle, onError, onFinally }) => {
+        set({ isLoading: true })
+        try {
+          await handle()
+        } catch (error: any) {
+          if (onError != null) {
+            return onError(error)
+          }
+        } finally {
+          set({ isLoading: false })
+          if (onFinally) onFinally()
+        }
+      }
+    }),
+    {
+      name: 'auth-storage',
+      storage: createJSONStorage(() => localStorage)
     }
-
-    const onError = async (): Promise<void> => {
-      console.log('Usuário ou senha inválidos');
-    }
-
-    void makeAsync({ handle, onError });
-  },
-
-  logout: () => {
-    set({ tokens: null });
-  },
-
-  makeAsync: async ({ handle, onError, onFinally }) => {
-    set({ isLoading: true });
-    try {
-        await handle();
-    } catch (error:any) {
-       
-        if (onError != null) { 
-            if (error.response.data.message.includes('Number of attempts exceeded')) { 
-                return (
-                  alert('Número de tentativas excedidas, tente novamente mais tarde')
-                )
-              }
-              return onError(error);
-        } 
-        
-    } finally {
-        set({ isLoading: false });
-        if (onFinally) onFinally();
-    }
-},
-}), 
-{
-  name: 'auth-storage',
-  storage: createJSONStorage(() => localStorage)
-}
+  )
 )
-)
+
+injectZustandInstance(useAuthStore)
+export default useAuthStore
